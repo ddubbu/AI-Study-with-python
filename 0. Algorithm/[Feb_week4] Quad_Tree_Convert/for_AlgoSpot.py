c393d24977f8b8f45ec49a3570824dc89050dab7
ddubbu <hello.ddubbu@gmail.com>
2020-02-13 18:57:08 +0900

    드디어 알고스팟 풀어내다!

--- 0. Algorithm/[Feb_week4] Quad_Tree_Convert/for_AlgoSpot.py
+++ 0. Algorithm/[Feb_week4] Quad_Tree_Convert/for_AlgoSpot.py
@@ -1,3 +1,5 @@
+import sys
+
 def init_node(value):
     node = [value, [None, None, None, None]]
     return node
@@ -18,29 +20,14 @@ def make_tree(ptr, string, idx):  # recursion
     return idx
 
 
-# DFS, stack(대신, 4번째 child 부터 스택에 넣기)
-def print_tree(ptr, stack):
+# DFS, stack
+def print_convert_tree(ptr, stack):
     if len(stack) == 0 and ptr[0] != None:
         stack.append(ptr)  # 포인터를 넣기
     while len(stack) != 0:  # 스택이 비지 않을 때까지 돌리기
         pop = stack.pop()
         if pop != None:
             print(pop[0], end="")
-        if pop[0] == "x":  # 4번째 아이부터 넣으면, 1번째 아이부터 출력됨.
-            # 정상 읽는 순서 : 1->2->3->4
-            stack.append(pop[1][3])
-            stack.append(pop[1][2])
-            stack.append(pop[1][1])
-            stack.append(pop[1][0])
-
-def print_convert_tree(wfile, ptr, stack):
-    if len(stack) == 0 and ptr[0] != None:
-        stack.append(ptr)  # 포인터를 넣기
-    while len(stack) != 0:  # 스택이 비지 않을 때까지 돌리기
-        pop = stack.pop()
-        if pop != None:
-            wfile.write(pop[0])
-            # print(pop[0], end="")
         if pop[0] == "x":
             # 정상 읽는 순서(idx): 0(왼쪽위)->1(오른쪽위)->2(왼쪽아래)->3(오른쪽아래)
             # 이미지 반대로 읽기 : 2->3->0->1 (나중에 읽는 아이를 스택에 먼저 넣기)
@@ -52,9 +39,8 @@ def print_convert_tree(wfile, ptr, stack):
 
 if __name__ == "__main__":
     # set read/write file variables
-    rfile = open("input.txt", mode="r")
-    wfile = open("output.txt", mode="wt", encoding="utf-8")
-
+    # rfile = open("input.txt", mode="r")
+    rfile = sys.stdin
     total_case = int(rfile.readline())
     for case in range(1, total_case + 1):
         # print("#", case)
@@ -62,8 +48,8 @@ if __name__ == "__main__":
         T = init_node(None)
         make_tree(T, string, 0)
         # print_tree(T, [])
-        print_convert_tree(wfile, T, [])
-        wfile.write("\n")
+        print_convert_tree(T, [])
+        print()
 
 
 
